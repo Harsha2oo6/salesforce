2b0c6324a8cc554781052c3943e370bb5465b0f3
harsha <rekhaharshavardhan11@gmail.com>
2026-01-12 10:25:54 +0530

    Remove CustomAssignbutton component and associated files to streamline project structure and eliminate unused code.

--- force-app/main/default/lwc/customAssignbutton/customAssignbutton.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CustomAssignbutton extends LightningElement {
-    handleAssign() {
-        console.log('Assign button clicked');
-    }
-}
